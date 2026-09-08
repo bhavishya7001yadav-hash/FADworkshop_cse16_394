44e2972ee4f272aaa4041b7b034ddb71a484d8ee
Bhavishya Yadav <bhavishya7001yadav@gmail.com>
2026-09-08 10:58:20 +0530

    commitlab

---
+++ lab1/domevent.js
@@ -0,0 +1,39 @@
+ import { EventEmitter } from "node:events";
+
+function createDOMElement()  {
+    const emitter = new EventEmitter();
+
+return {
+ 
+    addEventListener(eventName, callback) {
+       emitter.on(eventName , callback);
+    },
+
+    removeEventListener(eventName, callback){
+        emiiter.off(eventName, callback);
+    },
+
+    dispatchEvent(event) {
+    emitter.emit(event.type, event);
+    },
+  };
+}
+
+const button = createDOMElement();
+button.addEventListener('click',()=>{
+    console.log("button clicked");
+})
+
+function handleClick(event) {
+    console.log("Second handler:", event.detail);
+}
+
+
+button.addEventListener("click,handleClick");
+
+button.dispatchEvent({
+    typr:"click",
+    detail:"Hello from node.js",
+});
+
+button.removeEventListener("click", handleClick);
